54abf1f70d7b439903e175a277bcfa7fdcf8345b
woo00oo <96woo94@naver.com>
2021-07-07 15:29:52 +0900

    타임리프 : 반복

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -105,4 +105,19 @@ public class BasicController {
     public String attribute() {
         return "basic/attribute";
     }
+
+    @GetMapping("/each")
+    public String each(Model model){
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model){
+        List<User> list = new ArrayList<>();
+        list.add(new User("UserA", 10));
+        list.add(new User("UserB", 20));
+        list.add(new User("UserC", 30));
+
+        model.addAttribute("users",list);
+    }
 }
