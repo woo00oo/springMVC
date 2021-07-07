d47109cca345a8df128db2ec8118fad59babffd2
woo00oo <96woo94@naver.com>
2021-07-07 11:00:36 +0900

    타임리프 : 연산

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -93,4 +93,11 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/literal";
     }
+
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
 }
